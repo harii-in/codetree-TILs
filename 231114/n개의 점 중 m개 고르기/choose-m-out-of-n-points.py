de0beee0baec73c8bbc5665bd6f23541fb4626e7
harii-in <76839618+harii-in@users.noreply.github.com>
2023-11-14 03:50:15 +0900

    Title: n개의 점 중 m개 고르기, Time: 207ms, Memory: 25MB, Status: Passed - CodeTree

--- 231114/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
+++ 231114/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -10,6 +10,6 @@ ans = 1e9
 for points in combinations(location, m):
     # 유클리디안 거리 중 최소
     # 거리가 가장 먼 두 점의 최소 거리 -> 거리가 가장 먼 두 점의 유클라디안 거리
-    distance = max(math.dist(p1, p2) for p1, p2 in combinations(points, 2))
+    distance = max((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2 for p1, p2 in combinations(points, 2))
     ans = min(ans, distance)
-print(int(ans)**2)
+print(int(ans))
