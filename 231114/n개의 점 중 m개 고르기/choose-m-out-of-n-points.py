392f07927efca754fd17427a1770782745c539e7
harii-in <76839618+harii-in@users.noreply.github.com>
2023-11-14 03:46:57 +0900

    Title: n개의 점 중 m개 고르기, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 231114/n개의 점 중 m개 고르기/choose-m-out-of-n-points.py
@@ -0,0 +1,15 @@
+import math
+from itertools import combinations
+
+n, m = map(int, input().split())
+location = [list(map(int, input().split())) for _ in range(n)]
+location.sort(key=lambda x: x[0])
+
+ans = 1e9
+# 점 m개 선택
+for points in combinations(location, m):
+    # 유클리디안 거리 중 최소
+    # 거리가 가장 먼 두 점의 최소 거리 -> 거리가 가장 먼 두 점의 유클라디안 거리
+    distance = max(math.dist(p1, p2) for p1, p2 in combinations(points, 2))
+    ans = min(ans, distance)
+print(int(ans)**2)
