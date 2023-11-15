d99cc50c8fc59d6a4646eedf21a70d65c855da3f
harii-in <76839618+harii-in@users.noreply.github.com>
2023-11-16 03:52:55 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 135ms, Memory: 26MB, Status: Passed - CodeTree

--- 231116/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
+++ 231116/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.py
@@ -6,11 +6,14 @@ n, m = map(int, input().split())
 
 # 뱀이 없는 경우 1, 뱀이 있는 경우 0
 grid = [list(map(int, input().split())) for _ in range(n)]
+visited = [[False]*m for _ in range(n)]
 
 # 뱀에게 물리지 않고 탈출 가능한 경로가 있으면 1, 없으면 0을 출력
+flag = False
 def DFS(x, y):
-    flag = False
+    global flag
     if x == n-1 and y == m-1 and grid[x][y] == 1:
+        flag = True
         print(1)
         return
     
@@ -18,13 +21,11 @@ def DFS(x, y):
         nx = x + dx[d]
         ny = y + dy[d]
 
-        if 0 <= nx < n and 0 <= ny < n and grid[nx][ny] == 1:
+        if 0 <= nx < n and 0 <= ny < m and grid[nx][ny] == 1 and not visited[nx][ny]:
+            visited[nx][ny] = True
             DFS(nx, ny)
-            flag = True
-            break
-    
-    if not flag:
-        print(0)
-        return
-        
+
+visited[0][0] = True
 DFS(0, 0)
+if not flag:
+    print(0)
