dcf64987abb51b587423c610d18c665e0656f14b
harii-in <76839618+harii-in@users.noreply.github.com>
2023-11-21 02:58:26 +0900

    Title: 우리는 하나, Time: 145ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 231121/우리는 하나/we-are-the-one.py
@@ -0,0 +1,45 @@
+from collections import deque
+from itertools import combinations
+
+# 상, 좌, 우, 하
+dx = [-1, 0, 0, 1]
+dy = [0, -1, 1, 0]
+
+n, k, u, d = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+def possible(x, y, visited):
+    return in_range(x, y) and not visited[x][y]
+
+def BFS(starts):
+    visited = [[False]*n for _ in range(n)]
+
+    cnt = 0
+    for x, y in starts:
+        queue = deque()
+        queue.append((x, y))
+        visited[x][y] = True
+
+        while queue:
+            x, y = queue.popleft()
+            
+            for i in range(4):
+                nx = x + dx[i]
+                ny = y + dy[i]
+                
+                if possible(nx, ny, visited):
+                    if u <= abs(grid[nx][ny] - grid[x][y]) <= d:
+                        queue.append((nx, ny))
+                        visited[nx][ny] = True
+                        cnt += 1
+    return cnt
+
+cities = [(x, y) for x in range(n) for y in range(n)]
+max_cnt = 0
+for starts in combinations(cities, k):
+    max_cnt = max(max_cnt, BFS(starts))
+
+print(max_cnt+1)
