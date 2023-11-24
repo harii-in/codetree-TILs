80b732b3887ed385a67ee2e95cc04ec3a4c48b85
harii-in <76839618+harii-in@users.noreply.github.com>
2023-11-24 10:50:17 +0900

    Title: 피보나치 수, Time: 156ms, Memory: 24MB, Status: Passed - CodeTree

--- 231124/피보나치 수/fibonacci-number.py
+++ 231124/피보나치 수/fibonacci-number.py
@@ -1,10 +1,13 @@
 N = int(input())
 
-dp = [0] * (N+1)
-dp[1] = 1
-dp[2] = 1
+if N == 1 or N == 2: 
+    print(1)
+else:
+    dp = [0] * (N+1)
+    dp[1] = 1
+    dp[2] = 1
 
-for i in range(3, N + 1):
-    dp[i] = dp[i - 1] + dp[i - 2]
+    for i in range(3, N + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
 
-print(dp[N])
+    print(dp[N])
