46667429b15cfb1b521769c272e4e9975cbc2788
harii-in <76839618+harii-in@users.noreply.github.com>
2023-12-04 21:44:25 +0900

    Title: 알바로 부자 되기, Time: 282ms, Memory: 25MB, Status: Passed - CodeTree

--- 231204/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 231204/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -1,8 +1,13 @@
-n = int(input())
-jobs = [list(map(int, input().split())) for _ in range(n)]
-dp = [job[2] for job in jobs]  # dp[i]: maximum profit when doing the i-th job
+N = int(input())
 
-for i in range(1, n):
+# s는 알바가 시작되는 날짜
+# e는 알바가 끝나는 날짜
+# p는 해당 알바를 하여 얻을 수 있는 금액
+jobs = [list(map(int, input().split())) for _ in range(N)]
+# 각 날에 할 수 있는 일의 크기
+dp = [job[2] for job in jobs]
+
+for i in range(1, N):
     for j in range(i):
         if jobs[i][0] > jobs[j][1]:
             dp[i] = max(dp[i], dp[j] + jobs[i][2])
