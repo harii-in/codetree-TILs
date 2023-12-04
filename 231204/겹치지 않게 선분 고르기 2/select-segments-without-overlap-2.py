a0bc36eef56b28228ce476e8fb32f9be4bbbb3f9
harii-in <76839618+harii-in@users.noreply.github.com>
2023-12-04 21:20:19 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 5010ms, Memory: 28MB, Status: Time Limit Exceed - CodeTree

---
+++ 231204/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -0,0 +1,30 @@
+n = int(input())
+lines = [list(map(int, input().split())) for _ in range(n)]
+
+choices = []
+max_choice = 0
+
+def choice_line(depth):
+    global max_choice
+    max_choice = max(max_choice, len(choices))
+
+    for i in range(depth, n):
+        if is_overlapped(lines[i]):
+            continue
+        choices.append(lines[i])
+        choice_line(i + 1)
+        choices.remove(lines[i])
+
+def is_overlapped(line):
+    for choice in choices:
+        if choice[0] == line[0] or choice[1] == line[1]:
+            return True
+        if choice[0] > line[0] and choice[1] > line[1]:
+            continue
+        if choice[0] < line[0] and choice[0] < line[0]:
+            continue
+        return True
+    return False
+
+choice_line(0)
+print(max_choice)
