9c47e95c31fbe207795ffa91ad6504181d76226a
harii-in <76839618+harii-in@users.noreply.github.com>
2024-01-14 23:30:19 +0900

    Title: 십자 모양 폭발, Time: 190ms, Memory: 26MB, Status: Passed - CodeTree

--- 240114/십자 모양 폭발/cross-shape-bomb.py
+++ 240114/십자 모양 폭발/cross-shape-bomb.py
@@ -21,11 +21,15 @@ for d in range(4):
             grid[nx][ny] = 0
 
 # 중력 적용
-for i in range(n):
-    for j in range(n):
-        if in_range(i+1, j) and grid[i+1][j] == 0:
-            grid[i+1][j] = grid[i][j]
+# 중력 적용
+for j in range(n):
+    non_zero_elements = [grid[i][j] for i in range(n) if grid[i][j] != 0]
+    for i in range(n):
+        if i < n - len(non_zero_elements):
             grid[i][j] = 0
+        else:
+            grid[i][j] = non_zero_elements[i - (n - len(non_zero_elements))]
+            
 
 for row in grid:
     print(*row)
