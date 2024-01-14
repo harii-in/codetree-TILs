3c422ae8f08097fd7257d59c0755f7791c77e124
harii-in <76839618+harii-in@users.noreply.github.com>
2024-01-14 12:40:14 +0900

    Title: 배낭 채우기 2, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240114/배낭 채우기 2/knapsack-2.py
+++ 240114/배낭 채우기 2/knapsack-2.py
@@ -1,22 +1,11 @@
-def knapsack_max_value(N, M, jewels):
-    # dp 배열 초기화
-    dp = [0] * (M + 1)
-
-    # 각 보석에 대해 최대 가치 갱신
-    for i in range(N):
-        w, v = jewels[i]
-        for weight in range(w, M + 1):
-            dp[weight] = max(dp[weight], dp[weight - w] + v)
-
-    return dp[M]
-
-# 입력 받기
 N, M = map(int, input().split())
-jewels = []
-for _ in range(N):
-    w, v = map(int, input().split())
-    jewels.append((w, v))
+jewels = [tuple(map(int, input().split())) for _ in range(N)]
 
-# 결과 출력
-result = knapsack_max_value(N, M, jewels)
-print(result)
+dp = [0] * (M + 1)
+
+for i in range(N):
+    w, v = jewels[i]
+    for weight in range(w, M + 1):
+        dp[weight] = max(dp[weight], dp[weight - w] + v)
+
+print(dp[M])
