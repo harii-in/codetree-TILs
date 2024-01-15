a9b8a39b70b0a79852b6a53ebd8e07dc0b05e275
harii-in <76839618+harii-in@users.noreply.github.com>
2024-01-15 16:22:44 +0900

    Title: 행복한 수열의 개수, Time: 132ms, Memory: 25MB, Status: Passed - CodeTree

--- 240115/행복한 수열의 개수/number-of-happy-sequence.py
+++ 240115/행복한 수열의 개수/number-of-happy-sequence.py
@@ -2,28 +2,32 @@ n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
 ans = 0
+flag = False
 
 # 행에 대한 행복한 수열 검사
 for row in grid:
-    tmp = 0
-    for i in range(1, n):
-        if row[i] == row[i-1]:
+    tmp = 1
+    flag = False
+
+    for i in range(n-1):
+        if row[i] == row[i+1]:
             tmp += 1
             if tmp >= m:
                 ans += 1
-                continue
+                break
         else:
             tmp = 1
 
+
 # 열에 대한 행복한 수열 검사
 for j in range(n):
-    tmp = 0
-    for i in range(1, n):
-        if grid[i][j] == grid[i-1][j]:
+    tmp = 1
+    for i in range(n-1):
+        if grid[i][j] == grid[i+1][j]:
             tmp += 1
             if tmp >= m:
                 ans += 1
-                continue
+                break
         else:
             tmp = 1
 
