1951c5727bb3530ce3856dbe0a88bad389b5209b
harii-in <76839618+harii-in@users.noreply.github.com>
2024-01-16 21:27:37 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 200ms, Memory: 26MB, Status: Passed - CodeTree

--- 240116/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240116/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -1,42 +1,38 @@
 n, m, t = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
+bash_positions = [[x-1, y-1] for _ in range(m) for x, y in [map(int, input().split())]]
 
-positions = [[x-1, y-1] for _ in range(m) for x, y in [map(int, input().split())]]
 
-dx = [0, 1, 0, -1]
-dy = [1, 0, -1, 0]
-
-def in_range(x, y):
+def in_range(x, y, n):
     return 0 <= x < n and 0 <= y < n
 
-def move(positions):
+
+def move_bash(n, grid, bash_positions):
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
     new_grid = [[0] * n for _ in range(n)]
 
-    for x, y in sorted(positions):
-        neighbor = []
-        # 상하좌우 중 가장 큰 값이 적혀있는 숫자
-        for d in range(4):
-            nx = x + dx[d]
-            ny = y + dy[d]
+    for x, y in sorted(bash_positions):
+        neighbor_values = []
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+            if in_range(nx, ny, n):
+                neighbor_values.append((grid[nx][ny], nx, ny))
 
-            if in_range(nx, ny):
-                neighbor.append((grid[nx][ny], nx, ny))
+        val, cur_x, cur_y = max(neighbor_values, key=lambda item: item[0])
+        new_grid[cur_x][cur_y] += 1
 
-        value, next_x, next_y = max(neighbor, key=lambda item: item[0])
-        new_grid[next_x][next_y] += 1
-
-    # 이동한 이후 2개 이상의 구슬 위치가 동일하면, 해당 위치에 있는 구슬들은 전부 사라짐
     for x in range(n):
         for y in range(n):
             if new_grid[x][y] >= 2:
                 new_grid[x][y] = 0
 
-    updated_position = [(x, y) for x in range(n) for y in range(n) if new_grid[x][y] == 1]
+    updated_bash_positions = [(x, y) for x in range(n) for y in range(n) if new_grid[x][y] == 1]
 
-    return updated_position
+    return updated_bash_positions
+
 
 
 for _ in range(t):
-    positions = move(positions)
+    bash_positions = move_bash(n, grid, bash_positions)
 
-print(len(positions))
+print(len(bash_positions))
