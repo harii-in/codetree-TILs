78c9abf5668424226d704ed12068924f854ec72d
harii-in <76839618+harii-in@users.noreply.github.com>
2024-01-16 21:34:12 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 242ms, Memory: 26MB, Status: Passed - CodeTree

--- 240116/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240116/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -2,7 +2,7 @@ n, m, t = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 positions = [[x-1, y-1] for _ in range(m) for x, y in [map(int, input().split())]]
 
-def in_range(x, y, n):
+def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
 
 # 상, 하, 좌, 우
@@ -11,16 +11,20 @@ dy = [0, 0, -1, 1]
 def move_bash(positions):
     new_grid = [[0] * n for _ in range(n)]
 
+    # 상하좌우로 인접한 곳에 있는 숫자들 중 가장 큰 값으로 이동
     for x, y in sorted(positions):
         neighbor = []
-        for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-            if in_range(nx, ny, n):
+        for d in range(4):
+            nx = x + dx[d]
+            ny = y + dy[d]
+            
+            if in_range(nx, ny):
                 neighbor.append((grid[nx][ny], nx, ny))
 
         val, cur_x, cur_y = max(neighbor, key=lambda item: item[0])
         new_grid[cur_x][cur_y] += 1
 
+    #  2개 이상의 구슬 위치가 동일하다면, 해당 위치에 있는 구슬들은 전부 사라짐
     for x in range(n):
         for y in range(n):
             if new_grid[x][y] >= 2:
@@ -29,7 +33,6 @@ def move_bash(positions):
     updated_positions = [(x, y) for x in range(n) for y in range(n) if new_grid[x][y] == 1]
 
     return updated_positions
-
 
 
 for _ in range(t):
