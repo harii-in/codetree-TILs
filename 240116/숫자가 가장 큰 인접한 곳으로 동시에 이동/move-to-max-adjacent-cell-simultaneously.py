ddaecdb45284dce9d3e1fffa062d6b6645e93034
harii-in <76839618+harii-in@users.noreply.github.com>
2024-01-16 21:31:22 +0900

    Title: 숫자가 가장 큰 인접한 곳으로 동시에 이동, Time: 203ms, Memory: 26MB, Status: Passed - CodeTree

--- 240116/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
+++ 240116/숫자가 가장 큰 인접한 곳으로 동시에 이동/move-to-max-adjacent-cell-simultaneously.py
@@ -5,18 +5,20 @@ positions = [[x-1, y-1] for _ in range(m) for x, y in [map(int, input().split())
 def in_range(x, y, n):
     return 0 <= x < n and 0 <= y < n
 
+# 상, 하, 좌, 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 def move_bash(positions):
-    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
     new_grid = [[0] * n for _ in range(n)]
 
     for x, y in sorted(positions):
-        neighbor_values = []
+        neighbor = []
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             if in_range(nx, ny, n):
-                neighbor_values.append((grid[nx][ny], nx, ny))
+                neighbor.append((grid[nx][ny], nx, ny))
 
-        val, cur_x, cur_y = max(neighbor_values, key=lambda item: item[0])
+        val, cur_x, cur_y = max(neighbor, key=lambda item: item[0])
         new_grid[cur_x][cur_y] += 1
 
     for x in range(n):
@@ -24,9 +26,9 @@ def move_bash(positions):
             if new_grid[x][y] >= 2:
                 new_grid[x][y] = 0
 
-    updated_bash_positions = [(x, y) for x in range(n) for y in range(n) if new_grid[x][y] == 1]
+    updated_positions = [(x, y) for x in range(n) for y in range(n) if new_grid[x][y] == 1]
 
-    return updated_bash_positions
+    return updated_positions
 
 
 
