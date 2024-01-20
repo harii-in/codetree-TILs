9d796805a60b6cab5a0d612017f723eb4fee2f1f
harii-in <76839618+harii-in@users.noreply.github.com>
2024-01-20 20:41:21 +0900

    Title: 금 채굴하기, Time: 473ms, Memory: 26MB, Status: Passed - CodeTree

--- 240120/금 채굴하기/gold-mining.py
+++ 240120/금 채굴하기/gold-mining.py
@@ -1,10 +1,10 @@
 n, m = map(int, input().split())
 grid = [list(map(int, input().split())) for _ in range(n)]
 
-def calPrice (k):
+def calPrice(k):
     return k * k + (k + 1) * (k + 1)
 
-def cntGold(x , y , k):
+def cntGold(x, y, k):
     total = 0
     for i in range(n):
         for j in range(n):
@@ -16,8 +16,8 @@ def cntGold(x , y , k):
 ans = 0
 for i in range(n):
     for j in range(n):
-        for k in range(n):
-            tmp = cntGold(i , j , k)
+        for k in range(n+1):
+            tmp = cntGold(i, j, k)
             if (tmp * m) >= calPrice(k) and ans < tmp:
                 ans = tmp
 
