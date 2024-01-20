1696fc9d9df5563d76d11ec6d5e33186a100e1ca
harii-in <76839618+harii-in@users.noreply.github.com>
2024-01-20 18:11:14 +0900

    Title: 계단 오르기 2, Time: 123ms, Memory: 25MB, Status: Passed - CodeTree

--- 240120/계단 오르기 2/climbing-stairs-2.py
+++ 240120/계단 오르기 2/climbing-stairs-2.py
@@ -1,14 +1,27 @@
 n = int(input())
-coins = list(map(int, input().split()))
+coin = [0] * (n + 1)
 
-dp = [0] * (n+1)
-for i in range(1, n+1):
-    if i == 1:
-        dp[i] = coins[i-1]
-    elif i == 2:
-        dp[i] = max(dp[i - 1], coins[i - 1])
-    else:
-        #  한 번에 1계단 오르는 것을 최대 3번까지 허용
-        dp[i] = max(dp[i - 1], dp[i - 2], dp[i - 3]) + coins[i - 1]
+# 각 층의 동전의 개수를 입력받습니다.
+coin = [0] + list(map(int, input().split()))
 
-print(dp[n])
+dp = [[0 for _ in range(5)] for _ in range(n + 1)]
+
+# 기본 케이스를 초기화합니다.
+dp[1][1] = coin[1]
+if n > 1:
+    #  두 번째 계단을 2계단 올라서 도달했을 떄의 동전의 수
+    dp[2][0] = coin[2]
+    # 1계단씩 두 번 올라 도달했을 때의 동전의 수
+    dp[2][2] = coin[1] + coin[2]
+
+# 동적 프로그래밍을 사용하여 최대 가치를 계산합니다.
+# dp[i][j] : i번 위치에 도착했을 때, 정확히 j번 1계단 올랐을 때의 최대 가치
+for i in range(3, n + 1):
+    for j in range(4):
+        dp[i][j] = max(dp[i][j], dp[i - 2][j] + coin[i])
+        if j:
+            dp[i][j] = max(dp[i][j], dp[i - 1][j - 1] + coin[i])
+
+# 가능한 모든 경우에서 최대 가치를 찾아 출력합니다.
+ans = max(dp[n])
+print(ans)
