fdd56301becd3a816d9d1f2257992d6f3ee49ca6
harii-in <76839618+harii-in@users.noreply.github.com>
2024-01-22 11:14:56 +0900

    Title: 1차원 폭발 게임, Time: 146ms, Memory: 24MB, Status: Passed - CodeTree

--- 240122/1차원 폭발 게임/The-1D-bomb-game.py
+++ 240122/1차원 폭발 게임/The-1D-bomb-game.py
@@ -5,13 +5,17 @@ while True:
     exploded = False
     to_remove = set()
 
+    # 현재 폭탄을 기준으로 M개의 폭탄을 확인
     for i in range(N - M + 1):
         count = 1
+        # 현재 폭탄을 제외한 그 다음 M-1개의 폭탄을 확인
         for j in range(1, M):
             if bombs[i + j] == bombs[i]:
                 count += 1
             else:
                 break
+        
+        # M개 이상의 연속된 숫자가 존재
         if count >= M:
             for j in range(M):
                 to_remove.add(i + j)
