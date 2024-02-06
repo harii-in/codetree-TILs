a509c7cc8b78b4c8e6debc13e7215d8a5a2ed02b
harii-in <76839618+harii-in@users.noreply.github.com>
2024-02-07 01:27:51 +0900

    Title: 숫자가 더 큰 인접한 곳으로 이동, Time: 116ms, Memory: 25MB, Status: Passed - CodeTree

--- 240207/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
+++ 240207/숫자가 더 큰 인접한 곳으로 이동/move-to-larger-adjacent-cell.py
@@ -1,28 +1,30 @@
 n, r, c = map(int, input().split())
-arr = [list(map(int, input().split())) for _ in range(n)]
+grid = [list(map(int, input().split())) for _ in range(n)]
 
-dxs = [-1, 1, 0, 0]
-dys = [0, 0, -1, 1]
+# 상, 하, 좌, 우
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 
-cur_x, cur_y = r - 1, c - 1
-compare_num = arr[cur_x][cur_y]
-visited_arr = [compare_num]
+x, y = r - 1, c - 1
+compare = grid[x][y]
+visited = [compare]
 
-def is_in_range(x, y):
+def inRange(x, y):
     return 0 <= x < n and 0 <= y < n
 
 def move():
-    global cur_x, cur_y, compare_num
-    for dx, dy in zip(dxs, dys):
-        next_x, next_y = cur_x + dx, cur_y + dy
-        if is_in_range(next_x, next_y) and arr[next_x][next_y] > compare_num:
-            compare_num = arr[next_x][next_y]
-            cur_x, cur_y = next_x, next_y
+    global x, y, compare
+    for d in range(4):
+        nx = x + dx[d]
+        ny = y + dy[d]
+        
+        if inRange(nx, ny) and grid[nx][ny] > compare:
+            compare = grid[nx][ny]
+            x, y = nx, ny
             return True
-
     return False
 
 while move():
-    visited_arr.append(arr[cur_x][cur_y])
+    visited.append(grid[x][y])
 
-print(*visited_arr)
+print(*visited)
