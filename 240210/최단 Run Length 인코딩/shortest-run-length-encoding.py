856ca6a03b9dd00e3b1b38be1a5e7b067e00b95a
harii-in <76839618+harii-in@users.noreply.github.com>
2024-02-10 01:37:44 +0900

    Title: 최단 Run Length 인코딩, Time: 138ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240210/최단 Run Length 인코딩/shortest-run-length-encoding.py
@@ -0,0 +1,52 @@
+def encode(target):
+    encoded = ""
+    encoded += target[0]
+    cnt = 1
+
+    for i in range(1, len(target)):
+        if target[i] == target[i - 1]:
+            cnt += 1
+            continue
+        encoded += str(cnt)
+        encoded += target[i]
+        cnt = 1
+
+    encoded += str(cnt)
+
+    return encoded
+
+
+def shift(target, cnt):
+    shifted = list(target)
+
+    while cnt > 0:
+        tmp = shifted[-1]
+        for i in range(len(shifted) - 1, 0, -1):
+            shifted[i] = shifted[i - 1]
+        shifted[0] = tmp
+        cnt -= 1
+
+    return ''.join(shifted)
+
+
+def get_min_len():
+    if n == 1:
+        return 2
+
+    min_len = float('inf')
+    old = A
+
+    for cnt in range(1, n):
+        shifted = shift(old, 1)
+        encoded = encode(shifted)
+        min_len = min(min_len, len(encoded))
+        old = shifted
+
+    return min_len
+
+
+if __name__ == "__main__":
+    A = input()
+    n = len(A)
+    min_len = get_min_len()
+    print(min_len)
