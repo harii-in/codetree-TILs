29f22ec0f26a0a1245cc82d120407abff54ca79a
harii-in <76839618+harii-in@users.noreply.github.com>
2024-02-16 21:38:27 +0900

    Title: 떨어지는 1자 블록, Time: 131ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240216/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240216/떨어지는 1자 블록/falling-horizontal-block.py
@@ -1,12 +1,9 @@
 n, m, k = tuple(map(int, input().split()))
 grid = [list(map(int, input().split())) for _ in range(n)]
 flag = False
-
 for row in range(n):
     for col in range(k - 1, k - 1 + m):
-        # 블럭이 존재하면
         if grid[row][col] == 1:
-            # 바로 위에 블럭 두기
             for j in range(k - 1, k - 1 + m):
                 grid[row - 1][j] = 1
             flag = True
@@ -16,8 +13,10 @@ for row in range(n):
         
 if not flag:
     for row in range(n):
-        for col in range(k - 1, k - 1 + m):
+        for col in range(k-1,k-1+m):
             grid[row][col] = 1
 
-for row in grid:
-    print(*row)
+for row in range(n):
+    for col in range(n):
+        print(grid[row][col], end=' ')
+    print()
