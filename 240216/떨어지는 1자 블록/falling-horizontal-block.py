2c54c7e56e200198797b57976d9125dfeb1b5029
harii-in <76839618+harii-in@users.noreply.github.com>
2024-02-16 21:33:45 +0900

    Title: 떨어지는 1자 블록, Time: 125ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240216/떨어지는 1자 블록/falling-horizontal-block.py
+++ 240216/떨어지는 1자 블록/falling-horizontal-block.py
@@ -3,6 +3,7 @@ grid = [list(map(int, input().split())) for _ in range(n)]
 flag = False
 r = 0
 
+# 블록이 떨어질 위치 찾기
 for row in range(n-1, -1, -1):
     for col in range(k - 1, k - 1 + m):
         if grid[row][col] == 1:
@@ -14,9 +15,11 @@ for row in range(n-1, -1, -1):
         r = row
         break
 
+# 블록이 떨어질 위치부터 m만큼의 영역에 블록 떨어뜨리기
 if flag:
     for col in range(k - 1, k - 1 + m):
         grid[r][col] = 1
 
+# 전체 격자판 상태 출력
 for row in grid:
     print(*row)
