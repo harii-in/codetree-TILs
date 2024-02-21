a960457d48b6a5fc6b40f1a9978ff5e9160913ed
harii-in <76839618+harii-in@users.noreply.github.com>
2024-02-22 03:11:40 +0900

    Title: 두 수의 합, Time: 163ms, Memory: 36MB, Status: Passed - CodeTree

--- 240222/두 수의 합/sum-of-two-num.py
+++ 240222/두 수의 합/sum-of-two-num.py
@@ -6,9 +6,11 @@ sum_dict = dict()
 for num in num_list:
     complement = k - num
 
+    # 가능한 모든 쌍의 수 세기
     if complement in sum_dict:
         cnt += sum_dict[complement]
 
+    # 현재 숫자의 개수 하나 증가
     if num in sum_dict:
         sum_dict[num] += 1
     else:
