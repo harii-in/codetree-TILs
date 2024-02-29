c53c64f85845a091900764db1c562869def7bb7a
harii-in <76839618+harii-in@users.noreply.github.com>
2024-03-01 02:43:05 +0900

    Title: 원소의 합이 0, Time: 340ms, Memory: 56MB, Status: Passed - CodeTree

--- 240301/원소의 합이 0/the-sum-of-the-elements-is-0.py
+++ 240301/원소의 합이 0/the-sum-of-the-elements-is-0.py
@@ -9,16 +9,20 @@ D = list(map(int, input().split()))
 AB = defaultdict(int)
 CD = defaultdict(int)
 
+# A 수열에서 숫자 하나, B 수열에서 숫자 하나를 골랐을 때
+# 나올 수 있는 두 숫자의 합들을 hashmap에 기록해줍니다.
 for a in A:
     for b in B:
         AB[a+b] += 1
 
+# C, D도 마찬가지
 for c in C:
     for d in D:
         CD[c+d] += 1
 
 ans = 0
 for ab in AB:
+    # CD에 -ab가 몇 개 들어 있는지를 찾기
     if -ab in CD:
         ans += AB[ab] * CD[-ab]
 
